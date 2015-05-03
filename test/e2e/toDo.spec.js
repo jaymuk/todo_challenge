37c856af6a4067f0faad104eeee1171752eaf272
jaymuk <jaymuk.business@gmail.com>
2015-05-03 13:47:56 +0100

    begin initialisation

---
+++ test/e2e/toDo.spec.js
@@ -0,0 +1,33 @@
+describe('To-Do List', function() {
+  it('can add a todo', function() {
+  
+  });
+});
+
+As a forgetful person
+I want to store my tasks
+So that I don't forget them
+
+As a person with limited time
+I want to instantly be able to update my todo list (adding and changing entries)
+So that I have more time to think about other things
+
+As a person who actually gets stuff done
+I want to mark my tasks as done
+So that I don't do them twice
+```
+
+Here are some other user stories you may choose to implement:
+
+```
+As a person with a lot of tasks
+I want to be able to filter my tasks by "All", "Active", "Complete"
+So that I only see the relevant tasks
+
+As a person who doesn't like counting by hand
+I want to see a total number of tasks
+So that I don't have to count
+
+As someone who has done lots of stuff
+I want to be able to clear my completed tasks
+So I never see them again
