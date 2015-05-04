e3a0c5b625a0bf97573fa9406da537ee4030e5ae
jaymuk <jaymuk.business@gmail.com>
2015-05-04 23:17:53 +0100

    need to add filtering

--- js/controllers/todoAppController.js
+++ js/controllers/todoAppController.js
@@ -4,6 +4,45 @@ todoApp.controller('todoAppController', [function() {
 
 
   self.addTask = function() {
-    self.tasks.push({'title': self.taskToAdd});
+    self.tasks.push({'title': self.taskToAdd, 'done':false, 'beingUpdated':false});
+    self.taskToAdd = '';
   };
+
+  self.deleteTask = function(index) {
+    self.tasks.splice(index, 1);
+  };
+
+  self.tasksRemaining = function() {
+    var remaining = 0;
+    for (var i = 0; i < self.tasks.length; i++) {
+      if (self.tasks[i].done === false) {
+        remaining += 1;
+      }
+    }
+    return remaining;
+  };
+
+  self.tasksComplete = function() {
+    var complete = 0;
+    for (var i = 0; i < self.tasks.length; i++) {
+      if (self.tasks[i].done === true) {
+        complete += 1;
+      }
+    }
+    return complete;
+  };
+
+  // self.updateTask = function() {
+  //   console.log('test');
+  //   $('.update').toggleClass('updater', [false, true]);
+  // };
+
+  // self.submitUpdate = function() {
+  //   console.log('update!!');
+  // };
+  
 }]);
+
+  // when I click on the list item button
+  // I want that list item to turn into an input box
+  // I want that input box to replace what was in that particular list element upon pressing enter
--- test/unit/todoAppController.spec.js
+++ test/unit/todoAppController.spec.js
@@ -19,4 +19,49 @@ describe('todoAppController', function() {
       expect(ctrl.tasks.length).toBe(1);
     });
   });
+
+    it('can delete that task', function() {
+      ctrl.taskToAdd = "Fake task";
+      ctrl.addTask();
+      expect(ctrl.tasks.length).toBe(1);
+      ctrl.deleteTask();
+      expect(ctrl.tasks.length).toBe(0);
+    });
+
+    it('can display total tasks added', function() {
+      ctrl.taskToAdd = "Fake task 1";
+      ctrl.addTask();
+      ctrl.taskToAdd = "Fake task 2";
+      ctrl.addTask();
+      expect(ctrl.tasksRemaining()).toBe(2);
+    });
+
+    it('can mark tasks as done', function() {
+      ctrl.taskToAdd = "Fake task 1";
+      ctrl.addTask();
+      ctrl.taskToAdd = "Fake task 2";
+      ctrl.addTask();
+      ctrl.tasks[1].done = true;
+      console.log(ctrl.tasks[1]);
+      expect(ctrl.tasksRemaining()).toBe(1);
+    });
+
+    xit('can update/change that task', function() {
+      ctrl.taskToAdd = "This task will be updated";
+      crl.addTask();
+      ctrl.updateTask("This task HAS BEEN updated");
+      expect(ctrl.tasks[0].title).toBe("This task HAS BEEN updated");
+    });
+
+  describe('user can filter tasks', function() {
+    it('to show only completed tasks', function() {
+      ctrl.taskToAdd = "Fake task 1";
+      ctrl.addTask();
+      ctrl.tasks[0].done = true;
+      ctrl.taskToAdd = "Fake task 2";
+      ctrl.addTask();
+
+    });
+  });
+
 });
